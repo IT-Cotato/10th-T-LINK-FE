35db1ae56ac0fa878aec01586099a58ceacc777e
yongaricode <kang061313@gmail.com>
2025-01-02 16:08:32 +0900

    feat: navBar 기능 추가

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,61 @@
-import 과외방 from '/과외방.png';
-import 캘린더 from '/캘린더.png';
-import 내정보 from '/내 정보.png';
+import { useState } from 'react';
+import { GoHome } from 'react-icons/go';
+import { GoHomeFill } from 'react-icons/go';
+import { useNavigate } from 'react-router-dom';
+
+// useLocationd을 사용해 경로로 접근하는 방식도 고려 필요
+// 경로로 접근할 일은 없으니 상관없나?
 
 const NavBar = () => {
+  const nav = useNavigate();
+  const [currentMenu, setCurrentMenu] = useState<string>('roomList');
+
   return (
     <div className="border-t border-gray-300 flex">
       <div className="py-2 flex justify-around w-full">
-        <div className="flex-col cursor-pointer">
-          <img src={과외방} className="w-[42px] h-[42px]"></img>
+        {/* 과외방 */}
+        <div
+          className="flex-col cursor-pointer"
+          onClick={() => {
+            setCurrentMenu('roomList');
+            nav('roomlist');
+          }}
+        >
+          {currentMenu == 'roomList' ? (
+            <GoHomeFill className="w-[42px] h-[42px]" />
+          ) : (
+            <GoHome className="w-[42px] h-[42px]" />
+          )}
           <p className="text-center">과외방</p>
         </div>
-        <div className="flex-col cursor-pointer">
-          <img src={캘린더} className="w-[42px] h-[42px]"></img>
+        {/* 캘린더 */}
+        <div
+          className="flex-col cursor-pointer"
+          onClick={() => {
+            setCurrentMenu('calendar');
+            nav('calendar');
+          }}
+        >
+          {currentMenu == 'calendar' ? (
+            <GoHomeFill className="w-[42px] h-[42px]" />
+          ) : (
+            <GoHome className="w-[42px] h-[42px]" />
+          )}
           <p className="text-center">캘린더</p>
         </div>
-        <div className="flex-col cursor-pointer">
-          <img src={내정보} className="w-[42px] h-[42px]"></img>
+        {/* 내 정보 */}
+        <div
+          className="flex-col cursor-pointer"
+          onClick={() => {
+            setCurrentMenu('mypage');
+            nav('mypage');
+          }}
+        >
+          {currentMenu == 'mypage' ? (
+            <GoHomeFill className="w-[42px] h-[42px]" />
+          ) : (
+            <GoHome className="w-[42px] h-[42px]" />
+          )}
           <p className="text-center">내 정보</p>
         </div>
       </div>
