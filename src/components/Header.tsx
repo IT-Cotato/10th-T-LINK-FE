80c340ae4d7dbf1658efa56d971921aa3eba43f6
AnSui22 <ansui218@ewhain.net>
2025-01-03 00:23:16 +0900

    feat: header 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,16 @@
+import { useNavigate } from 'react-router-dom';
+import { GoArrowLeft } from 'react-icons/go';
+
 const Header = () => {
-  return <div>Hedaer</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="flex">
+      <button onClick={() => navigate(-1)}>
+        <GoArrowLeft />
+      </button>
+      <div>로고 이미지</div>
+    </div>
+  );
 };
 
 export default Header;
