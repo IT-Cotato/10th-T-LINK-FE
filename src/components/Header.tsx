18c2ae5df4107d534ca302cab1f60fb3ee432e4d
AnSui22 <ansui218@ewhain.net>
2025-01-04 16:59:36 +0900

    feat: 로고 없는 헤더 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,16 @@
+import { useNavigate } from 'react-router-dom';
+import { GoArrowLeft } from 'react-icons/go';
+
 const Header = () => {
-  return <div>Hedaer</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="flex items-center h-16">
+      <button className="bg-transparent" onClick={() => navigate(-1)}>
+        <GoArrowLeft size={24} />
+      </button>
+      <div className="flex-1"></div>
+    </div>
+  );
 };
 
 export default Header;
