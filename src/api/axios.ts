7d0937df54e974c9cad9945e3e1dcbdf04b2b62f
AnSui22 <ansui218@ewhain.net>
2025-01-11 21:11:47 +0900

    fix: 토큰 갱신 코드 수정

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -11,11 +11,10 @@ const instance: AxiosInstance = axios.create({
 
 instance.interceptors.request.use(
   (config) => {
-    // 요청이 전달되기 전에 헤더에 토큰 추가
-    // 추후에 바꿔도 될 듯 => 상의 필요
-    const accesstoken = localStorage.getItem('accesstoken');
-    if (accesstoken) {
-      config.headers.Authorization = `Bearer ${accesstoken}`;
+    // 요청이 전달되기 전 헤더에 토큰 추가
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
     }
     return config;
   },
@@ -31,25 +30,26 @@ instance.interceptors.response.use(
   async (error) => {
     // 토큰 만료 시
     const msg = error.response.data.msg; // 백엔드에서 토큰 만료됐다고 알려주는 msg("Expired Access Token")
+    const refreshToken = localStorage.getItem('refreshToken');
     if (error.response.status === 401 && msg === 'Expired Access Token') {
       try {
-        const res = await axios.post(
-          '/백에서 주는 url',
-          {},
-          {
-            headers: {
-              accessToken: `${localStorage.getItem('accessToken')}`,
-              refreshToken: `${localStorage.getItem('refreshToken')}`,
+        if (refreshToken) {
+          const res = await axios.post(
+            '/백에서 주는 url',
+            { refreshToken },
+            {
+              headers: {
+                'Content-Type': 'application/json',
+              },
             },
-          },
-        );
-        localStorage.setItem('accessToken', res.headers.accessToken);
-        localStorage.setItem('refreshToken', res.headers.refreshToken);
+          );
+          localStorage.setItem('accessToken', res.headers.accessToken);
 
-        error.config.headers.Authorization = `Bearer ${res.headers.accessToken}`;
-        return axios(error.config);
+          error.config.headers.Authorization = `Bearer ${res.headers.accessToken}`;
+          return axios(error.config);
+        }
       } catch (refreshErr) {
-        console.log(refreshErr);
+        console.log('Token 갱신 실패: ', refreshErr);
 
         localStorage.removeItem('accessToken');
         localStorage.removeItem('refreshToken');
