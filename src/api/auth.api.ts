eabc05673da142e685dc54a08140932f6028013b
AnSui22 <ansui218@ewhain.net>
2025-01-13 13:39:49 +0900

    feat: roomList api 생성

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -7,11 +7,13 @@ interface UserInfo {
   birthday: string;
 }
 
+// 카카오 로그인 인가코드 보내기
 export const postAuthCode = async (code: string) => {
   const res = await instance.post('/api/auth/kakao/signin', code);
   return res.data;
 };
 
+//회원가입 시 입력한 정보 보내기
 export const postUserInfo = async (userInfo: UserInfo) => {
   const res = await instance.post('/api/v1/user', userInfo);
   return res.data;
