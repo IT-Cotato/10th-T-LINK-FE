5bf05b3ec8c36aa5c4c7eb63bcdb10f1dcb22004
AnSui22 <ansui218@ewhain.net>
2025-01-17 00:46:31 +0900

    feat: 입력안한 값이 있을 경우 버튼 disabled로 설정

--- src/pages/SignupForm.tsx
+++ src/pages/SignupForm.tsx
@@ -79,6 +79,7 @@ const SignupForm = () => {
       </div>
       <div className="px-5 py-6 flex justify-center fixed bottom-0 max-w-[500px] w-full">
         <button
+          disabled={Object.values(userInput).some((value) => value === '')}
           className="flex-1 px-4 py-3.5 text-gray-500 bg-gray-100 enabled:text-white enabled:bg-primary_700 font-semibold"
           onClick={handleStart}
         >
