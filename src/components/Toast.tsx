480a0c34aa0ac83d3ce71068e10f4afcc02f70c8
AnSui22 <ansui218@ewhain.net>
2025-01-24 00:02:20 +0900

    feat: 초대 링크 및 복사 구현

---
+++ src/components/Toast.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import { setTime } from 'react-datepicker/dist/date_utils';
+
+type ToastProps = {
+  setToast: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Toast = ({ setToast }: ToastProps) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setToast(false);
+    }, 1500);
+
+    return clearTimeout(timer);
+  }, [setToast]);
+
+  return <div className="border-2">클립보드에 복사되었습니다</div>;
+};
+
+export default Toast;
