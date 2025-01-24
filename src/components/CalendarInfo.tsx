7089a4a14b0dad5e6c46118874c8d3146628d8f4
AnSui22 <ansui218@ewhain.net>
2025-01-25 01:27:52 +0900

    feat: 캘린더 클릭 시 하단 부분 구현

---
+++ src/components/CalendarInfo.tsx
@@ -0,0 +1,26 @@
+import { Clicked } from '../models/calendar.model';
+import CalendarLesson from './CalendarLesson';
+import CalendarDeposit from './CalendarDeposit';
+import CalendarNolesson from './CalendarNolesson';
+
+type CalendarProps = {
+  clickDate: Clicked;
+};
+
+const CalendarInfo = ({ clickDate }: CalendarProps) => {
+  console.log(clickDate);
+  return (
+    <div>
+      <div className="flex text-base pt-4 pb-2">
+        {clickDate.month}월 {clickDate.day}일
+      </div>
+      <div className="flex flex-col gap-2">
+        {clickDate.roomname && <CalendarDeposit roomname={clickDate.roomname} />}
+        {clickDate.subjectAndRoom && <CalendarLesson subjectAndRoom={clickDate.subjectAndRoom} />}
+        {!clickDate.roomname && !clickDate.subjectAndRoom && <CalendarNolesson />}
+      </div>
+    </div>
+  );
+};
+
+export default CalendarInfo;
---
+++ src/components/CalendarLesson.tsx
@@ -0,0 +1,25 @@
+import user_blue from '../assets/images/user_blue.png';
+import vector from '../assets/images/vector_gray.png';
+
+type LessonProps = {
+  subjectAndRoom: string;
+};
+
+const CalendarLesson = ({ subjectAndRoom }: LessonProps) => {
+  return (
+    <div className="flex p-4 gap-3 bg-gray-50 items-center rounded-xl">
+      <div className="p-1.5 rounded-full bg-white">
+        <img src={user_blue} className="w-7 h-7" />
+      </div>
+      <div>
+        <img src={vector} className="w-1 h-6" />
+      </div>
+      <div>
+        <div className="text-gray-900 text-base font-semibold leading-7">{subjectAndRoom}</div>
+        <div className="text-sm leading-6">수업일 입니다.</div>
+      </div>
+    </div>
+  );
+};
+
+export default CalendarLesson;
