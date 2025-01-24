e77bb036a0f2c027ed86ffc2ec56e9c2c575421e
AnSui22 <ansui218@ewhain.net>
2025-01-25 01:25:53 +0900

    fix: 요일 순서 수정

--- src/utils/LessonDaysList.ts
+++ src/utils/LessonDaysList.ts
@@ -1,11 +1,11 @@
 import { LessonDay } from '../models/room.model';
 
 export const LessonDaysList: LessonDay[] = [
-  { id: 0, lessonDay: '월' },
-  { id: 1, lessonDay: '화' },
-  { id: 2, lessonDay: '수' },
-  { id: 3, lessonDay: '목' },
-  { id: 4, lessonDay: '금' },
-  { id: 5, lessonDay: '토' },
-  { id: 6, lessonDay: '일' },
+  { id: 0, lessonDay: '일' },
+  { id: 1, lessonDay: '월' },
+  { id: 2, lessonDay: '화' },
+  { id: 3, lessonDay: '수' },
+  { id: 4, lessonDay: '목' },
+  { id: 5, lessonDay: '금' },
+  { id: 6, lessonDay: '토' },
 ];
