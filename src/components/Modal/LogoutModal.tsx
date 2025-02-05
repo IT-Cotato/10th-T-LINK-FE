a797707547b022111176d98f59c36e0c5214e58c
AnSui22 <ansui218@ewhain.net>
2025-02-05 22:47:38 +0900

    feat: 로그아웃 기능 구현

--- src/components/Modal/LogoutModal.tsx
+++ src/components/Modal/LogoutModal.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import { postLogout } from '../../api/auth.api';
+import { useNavigate } from 'react-router-dom';
 
 interface LogoutModalProps {
   setModalOpen: (value: boolean) => void;
@@ -17,6 +19,7 @@ export const MODAL_TYPE = {
 } as const;
 
 const LogoutModal = ({ setModalOpen, type }: LogoutModalProps) => {
+  const navigate = useNavigate();
   const DEFAULT_TYPE = {
     text: '',
     buttonText: '',
@@ -42,9 +45,18 @@ const LogoutModal = ({ setModalOpen, type }: LogoutModalProps) => {
     }
   }, []);
 
-  const handleLogOut = () => {
+  const handleLogOut = async () => {
     // 로그아웃 로직
-    console.log('로그아웃');
+    try {
+      const res = await postLogout();
+      console.log(res);
+      localStorage.removeItem('accessToken');
+      localStorage.removeItem('refreshToken');
+    } catch (e) {
+      console.error(e);
+    } finally {
+      navigate('/');
+    }
   };
 
   const handleQuit = () => {
