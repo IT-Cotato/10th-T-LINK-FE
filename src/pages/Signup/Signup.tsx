078d5b40389704321e20f53a8afd1816900efd88
AnSui22 <ansui218@ewhain.net>
2025-02-08 21:27:53 +0900

    feat: 회원가입 로딩 페이지 UI

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -42,7 +42,7 @@ const Signup = () => {
           className="flex-1 px-4 py-3.5 text-gray-500 bg-gray-100 enabled:text-white enabled:bg-primary_700 font-semibold"
           onClick={handleStart}
         >
-          다음
+          시작하기
         </button>
       </div>
     </div>
