9e3d1b1497e42df783f832988ebb7b37e2afd1ac
yongaricode <kang061313@gmail.com>
2025-02-08 14:13:13 +0900

    feat: 과외방 상세 UI

--- src/pages/Room/RoomDetail.tsx
+++ src/pages/Room/RoomDetail.tsx
@@ -1,31 +1,38 @@
 import { useEffect, useState } from 'react';
 import DetailButton from '../../components/DetailButton';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { RoomDetails } from '../../models/room.model';
+import Edit from '../../assets/images/RoomDetail/Edit.svg?react';
+import CalendarDeposit from '../../components/Calendar/CalendarDeposit';
+import CalendarNolesson from '../../components/Calendar/CalendarNolesson';
 
 const RoomDetail = () => {
   const { roomId } = useParams<{ roomId: string }>();
   const [roomDetail, setRoomDetail] = useState<RoomDetails | null>(null);
+  const nav = useNavigate();
+
   useEffect(() => {
     getRoomInfo();
   }, [roomId]);
+
+  const userRole = localStorage.getItem('roleInfo');
 
   const getRoomInfo = async () => {
     try {
       // 확인용 mock data
       const mockResponse = {
         roomId: 1,
-        roomName: '대현동 중3',
+        roomName: '국어 이준서 학생',
         studentName: '다람쥐',
         subject: '수학',
-        lessonDays: [{ lessonDay: 'Monday' }, { lessonDay: 'Wednesday' }],
-        nextDepositDate: '2025-01-31',
+        lessonDays: [{ lessonDay: '월요일' }, { lessonDay: '수요일' }],
+        nextDepositDate: '25.01.31',
         permission: {
           lecture_file: true,
           homework: true,
           gradeStatistic: false,
           counselingLog: true,
-          deposit: false,
+          deposit: true,
         },
       };
 
@@ -43,29 +50,49 @@ const RoomDetail = () => {
   }
 
   return (
-    <div className="px-4 flex-col flex gap-5">
-      <h2>{roomDetail.roomName}</h2>
-      <p>{`학생 이름: ${roomDetail.studentName}`}</p>
-      <p>{`과목: ${roomDetail.subject}`}</p>
-      <h3>수업 요일</h3>
-      <ul>
-        {roomDetail.lessonDays.map((day, index) => (
-          <li key={index}>{day.lessonDay}</li>
-        ))}
-      </ul>
-      <div className="grid grid-cols-2 gap-4">
+    <div className="flex-col flex">
+      {/* 방 정보 */}
+      <div className="flex flex-col p-4 gap-2">
+        <div className="justify-between flex items-center">
+          <h2 className="text-heading6 font-bold">{roomDetail.roomName}</h2>
+          {userRole == 'TEACHER' ? <Edit onClick={() => nav('edit')} /> : ''}
+        </div>
+        <div className="flex flex-col tracking-[-0.042px]">
+          {/* 요일&과목명 */}
+          <div className="flex text-body4 font-medium gap-[6px] items-center">
+            <div>
+              <span className="text-gray-500">#</span>
+              <span className="text-primary_700">{roomDetail.subject}</span>
+            </div>
+            {roomDetail.lessonDays.map((day, index) => (
+              <li key={index} className="list-none">
+                <span className="text-gray-500">#</span>
+                <span className="text-primary_700">{day.lessonDay}</span>
+              </li>
+            ))}
+          </div>
+          <div className="flex gap-[6px] text-body4 text-gray-500 items-center">
+            <div className="flex gap-[2px]">
+              <span className="text-gray-950">{roomDetail.studentName}</span>
+              <span>학생</span>
+            </div>
+            <div className="h-[12px] bg-gray-500 w-[1px]" />
+            <p>25.02.04</p>
+          </div>
+        </div>
+      </div>
+      {/* 목록 */}
+      <div className="grid grid-cols-2 gap-2 pb-6 px-4 border-b-2 border-gray-100">
         <DetailButton type="materials" isPermission={roomDetail.permission.lecture_file} />
         <DetailButton type="homework" isPermission={roomDetail.permission.homework} />
         <DetailButton type="stats" isPermission={roomDetail.permission.gradeStatistic} />
         <DetailButton type="diary" isPermission={roomDetail.permission.counselingLog} />
       </div>
-      <DetailButton
-        type="payment"
-        nextDepositDate={roomDetail.nextDepositDate}
-        isPermission={roomDetail.permission.deposit}
-      />
-      {/* role 보고 선생님이면 isPermission true */}
-      <DetailButton type="sharecode" isPermission={true} />
+      {/* 입금 & 링크 */}
+      <div className="p-4 flex flex-col gap-2">
+        <CalendarDeposit nextDeopsit={roomDetail.nextDepositDate} isPermission={roomDetail.permission.deposit} />
+        {userRole == 'TEACHER' ? <CalendarNolesson isShareLink={true} /> : ''}
+      </div>
     </div>
   );
 };
