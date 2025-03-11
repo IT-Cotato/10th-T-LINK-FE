acdebfda2ba7eb206e116a08e212b81f9e6c8574
AnSui22 <ansui218@ewhain.net>
2025-03-11 14:39:08 +0900

    fix: 링크 복사 버튼 모달로 수정

--- src/components/Modal/ShareLinkModal.tsx
+++ src/components/Modal/ShareLinkModal.tsx
@@ -1,31 +1,18 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { getShareCode } from '../../api/roomList.api';
 import Toast from './Toast';
 import link from '../../assets/images/link.png';
-import copy from 'clipboard-copy';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 interface ShareLinkProps {
   setModalOpen: (value: boolean) => void;
-  roomId: number;
+  shareLink: string;
+  nav: boolean;
 }
 
-const ShareLinkModal = ({ roomId }: ShareLinkProps) => {
+const ShareLinkModal = ({ setModalOpen, shareLink, nav }: ShareLinkProps) => {
   const navigation = useNavigate();
   const [toast, setToast] = useState(false);
-
-  const handleCopy = async () => {
-    try {
-      const res = await getShareCode(roomId);
-      const shareCode = res.data.data.shareCode;
-      const link = `https://t-link.site/user/roomlist/invite/${roomId}/${shareCode}`;
-      console.log(link);
-      await copy(link);
-      setToast(true);
-    } catch (e) {
-      alert('failed');
-    }
-  };
 
   return (
     <>
@@ -34,15 +21,17 @@ const ShareLinkModal = ({ roomId }: ShareLinkProps) => {
           <p className="font-semibold text-lg leading-8">과외방 링크</p>
           <p className="font-normal text-sm leading-[22px] text-gray-600">학생에게 참여 링크를 공유해주세요.</p>
         </div>
-        <button className="flex flex-col items-center gap-1" onClick={() => handleCopy()}>
-          <div className="bg-gray-50 rounded-full p-2 ">
-            <img src={link} className="w-8 h-8" />
-          </div>
-          <p className="text-gray-500 text-xs font-normal leading-[22px]">링크 복사하기</p>
-        </button>
+        <CopyToClipboard text={shareLink} onCopy={() => setToast(true)}>
+          <button className="flex flex-col items-center gap-1">
+            <div className="bg-gray-50 rounded-full p-2 ">
+              <img src={link} className="w-8 h-8" />
+            </div>
+            <p className="text-gray-500 text-xs font-normal leading-[22px]">링크 복사하기</p>
+          </button>
+        </CopyToClipboard>
         <button
           className="flex-1 w-full text-[16px] font-semibold py-[14px] bg-white border border-gray-500 rounded-full box-border text-center"
-          onClick={() => navigation('/user/roomlist')}
+          onClick={() => (nav ? navigation('/user/roomlist') : setModalOpen(false))}
         >
           완료
         </button>
--- src/pages/Room/ShareCode.tsx
+++ src/pages/Room/ShareCode.tsx
@@ -1,19 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Modal from '../../components/Modal/Modal';
 import ShareLinkModal from '../../components/Modal/ShareLinkModal';
+import { getShareCode } from '../../api/roomList.api';
 
 const ShareCode = () => {
   const location = useLocation();
   const roomId = location.state.roomId;
-
   const [modalOpen, setModalOpen] = useState(true);
+  const [link, setLink] = useState('');
+
+  useEffect(() => {
+    const handleLink = async () => {
+      try {
+        const res = await getShareCode(Number(roomId));
+        const shareCode = res.data.data.shareCode;
+        setLink(`https://t-link.site/user/roomlist/invite/${roomId}/${shareCode}`);
+        setModalOpen(true);
+      } catch (e) {
+        alert('초대 코드를 가져오는데 실패했습니다.');
+      }
+    };
+
+    handleLink();
+  }, []);
+
+  useEffect(() => {
+    console.log(link);
+  }, [link]);
 
   return (
     <div className="flex py-5 flex-col h-full justify-center items-center ">
       {modalOpen && (
-        <Modal onClose={() => setModalOpen(false)}>
-          <ShareLinkModal setModalOpen={setModalOpen} roomId={roomId} />
+        <Modal>
+          <ShareLinkModal setModalOpen={setModalOpen} shareLink={link} nav={true} />
         </Modal>
       )}
     </div>
