d31c27545e3683fdf5bd4ffdc464de8c49bf7e6b
yongaricode <kang061313@gmail.com>
2025-03-16 17:27:53 +0900

    refactor: 강의 자료 생성 코드 정리

--- src/pages/Material/CreateMaterials.tsx
+++ src/pages/Material/CreateMaterials.tsx
@@ -1,10 +1,12 @@
-import { ChangeEvent, useState } from 'react';
+import { useState } from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
 import { useNavigate, useParams } from 'react-router-dom';
 import { uploadLectureFile } from '../../api/materials.api';
 import LongButton from '../../components/Common/LongButton';
 import AddFile from '../../components/Material/AddFile';
 import Input from '../../components/RoomDetail/Input';
+import Container from '../../components/Common/Container';
+import CreateDesc from '../../components/RoomDetail/CreateDesc';
 
 const CreateMaterials = () => {
   const { roomId } = useParams<{ roomId: string }>();
@@ -33,28 +35,15 @@ const CreateMaterials = () => {
       lectureFiles: fileList,
     };
 
-    // api 호출
-    try {
-      const response = await uploadLectureFile(roomId!, payload);
-      if (response.status == 201) {
-        nav(-1);
-      }
-    } catch (error) {
-      console.log('강의 자료 업로드 실패', error);
-    }
+    // 숙제 업로드
+    await uploadLectureFile(roomId!, payload);
+    nav(-1);
   };
 
   return (
-    <div className="px-4 flex flex-col h-full">
+    <Container>
       {/* 설명 */}
-      <div className="py-4">
-        <p className="text-heading6 font-bold leading-10 text-gray-900">
-          업로드 할 자료의 정보를 입력하세요.
-        </p>
-        <p className="text-body3 font-normal leading-7 tracking-[-0.048px] text-gray-600">
-          언제든지 수정할 수 있어요!
-        </p>
-      </div>
+      <CreateDesc title="업로드 할 자료의 정보를 입력하세요." desc="언제든지 수정할 수 있어요!" />
       {/* 파일첨부 */}
       <div className="py-4 gap-6 flex flex-col">
         <Input
@@ -66,7 +55,6 @@ const CreateMaterials = () => {
         />
         <AddFile fileList={fileList} setFileList={setFileList} />
       </div>
-
       <div className="py-6 mt-auto">
         <LongButton
           enable={!!(fileList.length > 0 && desc.length > 0)}
@@ -74,7 +62,7 @@ const CreateMaterials = () => {
           text="업로드 하기"
         />
       </div>
-    </div>
+    </Container>
   );
 };
 
