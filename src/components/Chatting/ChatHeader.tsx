d19c897f8c921820cc8d2083be4d8c51bace3052
yongaricode <kang061313@gmail.com>
2025-05-07 17:27:24 +0900

    feat: 채팅방 뒤로 가기 추가

--- src/components/Chatting/ChatHeader.tsx
+++ src/components/Chatting/ChatHeader.tsx
@@ -1,10 +1,13 @@
 import { GoArrowLeft } from 'react-icons/go';
 import MenuIcon from '../../assets/images/Chatting/Meatballs_Horizental menu.svg?react';
+import { useNavigate } from 'react-router-dom';
 
 const ChatHeader = () => {
+  const nav = useNavigate();
+
   return (
     <div className="absolute top-0 max-w-[500px] w-full flex py-2 px-[6px] justify-around items-center border-b-2 border-gray-100">
-      <GoArrowLeft size={24} className="m-[10px]" />
+      <GoArrowLeft size={24} className="m-[10px]" onClick={() => nav(-1)} />
       <div className="flex items-center gap-2 flex-1 justify-center">
         <img
           src="https://mblogthumb-phinf.pstatic.net/MjAyMDAyMTBfODAg/MDAxNTgxMzA0MTE3ODMy.ACRLtB9v5NH-I2qjWrwiXLb7TeUiG442cJmcdzVum7cg.eTLpNg_n0rAS5sWOsofRrvBy0qZk_QcWSfUiIagTfd8g.JPEG.lattepain/1581304118739.jpg?type=w800"
